404a205b15d20aab71670518b49a21695f975890
Chris McCarthy <chrismccarthy@ChrissMacStudio.attlocal.net>
2025-06-12 08:23:25 -0500

    Allow iframe embedding from WordPress

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+        ],
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
